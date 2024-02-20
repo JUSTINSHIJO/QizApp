09bbd0a99f0c6c7b330cab632120586e10f48b47
midhunsaji10 <160301328+midhunsaji10@users.noreply.github.com>
2024-02-20 17:14:59 +0530

    initial

--- lib/question.dart
+++ lib/question.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Question extends StatefulWidget {
   const Question({Key? key}) : super(key: key);
 
@@ -10,7 +11,18 @@ class _QuestionState extends State<Question> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown,
-    );
+        backgroundColor: Colors.blueAccent,
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('do you have cofee?', style: TextStyle(fontSize: 30)),
+         TextButton(onPressed: (){}, child: Text('YES'),
+             style: ButtonStyle(
+               overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                       (Set<MaterialState> states) {
+                     if (states.contains(MaterialState.focused)) return Colors.red;
+                     return null; // Defer to the widget's default.
+                   }),
+             ) )
+
+        ],));
   }
 }
